cce883c989bfbcc00097188f1f7b8137c48cc01c
kanny <kanny.ghafour87@gmail.com>
2023-06-05 19:02:47 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(arr, name) {
+    const messages = [];
+    for (let i = 0; i < arr.length; i++) {
+        const message = `Thank you, ${arr[i]}, for the wonderful ${name} gift!`;
+        console.log(message);
+        messages.push(message);
+    }
+    return messages;
+}
+function countDown(num){
+    let i = num
+    while (i>=0){
+        console.log(i);
+        i--;
+    }
+}
